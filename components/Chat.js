27e0ecf2b7f94ccfbab6c7d0769e3fb89cddc47c
hostech83 <166859198+hostech83@users.noreply.github.com>
2024-12-16 21:19:39 -0500

    install and import Gifted Chat and render a GiftedChat component

--- components/Chat.js
+++ components/Chat.js
@@ -1,25 +1,80 @@
-import { useEffect } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from "react-native";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
+
 // Destructure name and background from route.params
 const Chat = ({ route, navigation }) => {
   const { name, background } = route.params;
-  // useEffect hook to set navigation options
+  const [messages, setMessages] = useState([]);
+
+  // Function to handle sending new messages
+  const onSend = (newMessages) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, newMessages)
+    );
+  };
+
+  // Function to customize the appearance of message bubbles
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: "#000",
+          },
+          left: {
+            backgroundColor: "#FFF",
+          },
+        }}
+      />
+    );
+  };
+
+  // Initialize messages when the component mounts
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
+  }, []);
+
+  // Update the navigation bar title when the `name` changes
   useEffect(() => {
     navigation.setOptions({ title: name });
-  }, []);
-  /* Render a View component with dynamic background color */
+  }, [name, navigation]);
+
+  // Render the chat interface
   return (
     <View style={[styles.container, { backgroundColor: background }]}>
-      <Text>Let's Chat!</Text>
+      <GiftedChat
+        messages={messages}
+        renderBubble={renderBubble}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+      {Platform.OS === "android" && <KeyboardAvoidingView behavior="height" />}
     </View>
   );
 };
+
 // Define styles for the component
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "Center",
     alignItems: "center",
   },
 });
+
 export default Chat;
