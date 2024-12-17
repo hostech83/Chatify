ad0e77a5b8f8f741019a2dea38caf222e3dab5ac
hostech83 <166859198+hostech83@users.noreply.github.com>
2024-12-17 09:27:32 -0500

    Add firebase Config

--- App.js
+++ App.js
@@ -2,12 +2,35 @@ import { StatusBar } from "expo-status-bar"; // Keep only one import
 import { StyleSheet } from "react-native";
 import Start from "./components/Start";
 import Chat from "./components/Chat";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+// Create the navigator
 const Stack = createNativeStackNavigator();
 
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyDfXIn81-Ds74KDmwJ5iKEEp0wkwcvJgaw",
+    authDomain: "chatify-6e517.firebaseapp.com",
+    projectId: "chatify-6e517",
+    storageBucket: "chatify-6e517.firebasestorage.app",
+    messagingSenderId: "38738128338",
+    appId: "1:38738128338:web:91fa3f427da6c269006c0b",
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
     /* Wrap the app with NavigationContainer */
     <NavigationContainer>
