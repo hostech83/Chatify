e1e804fbf33179d097134e6e637e71d76ef4a476
hostech83 <166859198+hostech83@users.noreply.github.com>
2025-01-07 21:03:31 -0500

    Add CustomActions & onActionPress

--- components/Chat.js
+++ components/Chat.js
@@ -15,6 +15,7 @@ import {
   query,
   orderBy,
 } from "firebase/firestore";
+import MapView from "react-native-maps";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import ContextDatabase from "../ContextDatabase";
 import CustomActions from "./CustomActions";
@@ -89,10 +90,18 @@ const Chat = ({ route, navigation, isConnected, storage }) => {
     return () => unsubscribeMessages && unsubscribeMessages();
   }, [isConnected, db, name, navigation]);
 
-  // Custom input toolbar based on connectivity
-  const renderInputToolbar = (props) =>
-    isConnected ? <InputToolbar {...props} /> : null;
-
+  //offline message in the input toolbar if there is no network connection
+  const renderInputToolbar = (props) => {
+    if (isConnected === true) {
+      return <InputToolbar {...props} />;
+    } else {
+      return (
+        <View style={styles.offlineInputToolbar}>
+          <Text style={styles.offlineTextInput}>No network connection</Text>
+        </View>
+      );
+    }
+  };
   // Customize the message bubble
   const renderBubble = (props) => (
     <Bubble
@@ -105,33 +114,91 @@ const Chat = ({ route, navigation, isConnected, storage }) => {
   );
 
   const renderCustomActions = (props) => {
-    return <CustomActions {...props} />;
+    return (
+      <CustomActions
+        {...props}
+        storage={storage}
+        userID={userID}
+        name={name}
+        onSend={onSend}
+        wrapperStyle={styles.customActionsWrapper}
+        iconTextStyle={styles.customActionsIconText}
+      />
+    );
   };
 
-  return (
-    <SafeAreaView style={[styles.container, { backgroundColor: background }]}>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        style={{ flex: 1 }}
-      >
-        <GiftedChat
-          messages={messages}
-          onSend={onSend}
-          renderActions={renderCustomActions}
-          renderBubble={renderBubble}
-          renderInputToolbar={renderInputToolbar}
-          user={{ _id: userID, name }}
+  const renderCustomView = (props) => {
+    const { currentMessage } = props;
+    if (currentMessage.location) {
+      return (
+        <MapView
+          style={{ width: 150, height: 100, borderRadius: 13, margin: 3 }}
+          region={{
+            latitude: currentMessage.location.latitude,
+            longitude: currentMessage.location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
         />
-      </KeyboardAvoidingView>
-    </SafeAreaView>
-  );
+      );
+    }
+
+    return (
+      <SafeAreaView style={[styles.container, { backgroundColor: background }]}>
+        {Platform.OS === "android" ? (
+          <KeyboardAvoidingView behavior="height" style={{ flex: 1 }}>
+            <GiftedChat
+              messages={messages}
+              onSend={onSend}
+              renderActions={renderCustomActions}
+              renderCustomView={renderCustomView}
+              renderBubble={renderBubble}
+              renderInputToolbar={renderInputToolbar}
+              user={{ _id: userID, name }}
+            />
+          </KeyboardAvoidingView>
+        ) : (
+          <GiftedChat
+            messages={messages}
+            onSend={onSend}
+            renderActions={renderCustomActions}
+            renderCustomView={renderCustomView}
+            renderBubble={renderBubble}
+            renderInputToolbar={renderInputToolbar}
+            user={{ _id: userID, name }}
+          />
+        )}
+      </SafeAreaView>
+    );
+  };
+
+  // Define styles
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    keyboardAvoidingView: {
+      flex: 1,
+    },
+    offlineTextInput: {
+      color: "black",
+      fontSize: 16,
+    },
+    offlineInputToolbar: {
+      backgroundColor: "lightgrey",
+      padding: 10,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    customActionsWrapper: {
+      alignSelf: "center",
+      marginLeft: 0,
+      marginRight: 4,
+      marginBottom: 0,
+    },
+    customActionsIconText: {
+      fontSize: 20,
+    },
+  });
 };
-
-// Define styles
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
 export default Chat;
